00369f018587670024e51ac1ed9da220f1268b7a
Jacob Clarke <jacob@bigfish.tv>
2016-08-19 18:37:15 +1000

    Envelope nodes are contains with mouse listeners now

--- src/timbre/components/Envelope.js
+++ src/timbre/components/Envelope.js
@@ -27,9 +27,49 @@ class Envelope extends Component {
 		this.graphics = new Graphics();
 		this.stage.addChild(this.graphics);
 
+		this.attackNode = this.generateNode('attack');
+		this.stage.addChild(this.attackNode);
+		this.decayNode = this.generateNode('decay');
+		this.stage.addChild(this.decayNode);
+		this.sustainNode = this.generateNode('sustain');
+		this.stage.addChild(this.sustainNode);
+		this.releaseNode = this.generateNode('release');
+		this.stage.addChild(this.releaseNode);
 
+		this.activeNode = null;
 		this.mounted = true;
 		this.animate();
+	}
+
+	generateNode(nodeName) {
+		const node = new Container();
+		const graphic = new Graphics();
+
+		node.id = nodeName;
+		node.interactive = true;
+		node.buttonMode = true;
+
+		graphic.beginFill(0xFFFFFF);
+		graphic.drawCircle(0, 0, 6);
+		node.graphic = graphic;
+		node.addChild(graphic);
+		
+		node.on('mouseover', () => this.activeNode = node);
+		node.on('mouseout', () => {
+			if(!this.placing) this.activeNode = null;
+		});
+		node.on('mousedown', () => {
+			this.activeNode = node;
+			this.placing = true;
+			this.mouseMoved = false;
+		})
+		node.on('mouseup', () => {
+			this.placing = false;
+			this.mouseMoved = false;
+			this.activeNode = null;
+		});
+
+		return node;
 	}
 
 	animate() {
@@ -48,9 +88,9 @@ class Envelope extends Component {
 		this.graphics.lineTo(point4.x, point4.y);
 		this.graphics.lineTo(point5.x, point5.y);
 		this.graphics.beginFill(0xFFFFFF);
-		this.graphics.drawCircle(point2.x, point2.y, 8);
-		this.graphics.drawCircle(point3.x, point3.y, 8);
-		this.graphics.drawCircle(point5.x, point5.y, 8);
+		this.attackNode.position.set(point2.x, point2.y);
+		this.decayNode.position.set(point3.x, point3.y);
+		this.releaseNode.position.set(point5.x, point5.y);
 
 		this.renderer.render(this.stage);
 		if(this.props.animating) requestAnimationFrame(this.animate.bind(this));
