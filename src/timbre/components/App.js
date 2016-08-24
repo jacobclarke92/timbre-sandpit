3a99aaf5104384c7568fcc2df1a6834319b3daa0
Jacob Clarke <jacob@bigfish.tv>
2016-08-24 18:54:09 +1000

    Bind number keys to ui views

--- src/timbre/components/App.js
+++ src/timbre/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import { STAGE, OSCILLATORS, MAPPINGS, FX } from '../constants/uiViews'
+import { addKeyListener } from '../utils/keyUtils'
+import { changeView } from '../reducers/gui'
 
 import Envelope from './Envelope'
 import TopUI from './TopUI'
@@ -12,6 +14,14 @@ import MappingsInterface from './MappingsInterface'
 import FxInterface from './FxInterface'
 
 class App extends Component {
+
+	componentDidMount() {
+		addKeyListener('1', () => this.props.dispatch(changeView(STAGE)));
+		addKeyListener('2', () => this.props.dispatch(changeView(OSCILLATORS)));
+		addKeyListener('3', () => this.props.dispatch(changeView(MAPPINGS)));
+		addKeyListener('4', () => this.props.dispatch(changeView(FX)));
+	}
+
 	render() {
 		const { view } = this.props.gui || {};
 		return(
