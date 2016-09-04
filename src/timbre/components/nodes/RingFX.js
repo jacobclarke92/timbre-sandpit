a6e23d3a9d3856415535bb457a3f7137213a4ed4
Jacob Clarke <jacobclarke92@gmail.com>
2016-09-04 23:59:00 +1000

    Higher resolution ring fx

--- src/timbre/components/nodes/RingFX.js
+++ src/timbre/components/nodes/RingFX.js
@@ -9,15 +9,15 @@ export default class RingFX extends Component {
 		super(props);
 		this.counter = 0;
 		this.ring = new Graphics();
-		this.ring.lineStyle(3, props.color, 1);
-		this.ring.drawCircle(0, 0, BEAT_PX*3);
+		this.ring.lineStyle(6, props.color, 1);
+		this.ring.drawCircle(0, 0, BEAT_PX*12);
 		this.ring.cacheAsBitmap = true;
 		this.ring.scale.set(0);
 		this.ring.position = props.position;
 	}
 
 	render() {
-		this.ring.scale.set(this.ring.scale.x + (this.props.bpm/11000)*this.props.speed); // just eyeballed this one
+		this.ring.scale.set(this.ring.scale.x + (this.props.bpm/44000)*this.props.speed); // just eyeballed this one
 		if(++this.counter >= 60) {
 			if(this.ring.alpha > 0) this.ring.alpha -= 0.05;
 			else this.props.onRemoveRingFX();
