3782f6cbb95201f9ebe492d7591f1c85b3058d52
Jacob Clarke <jacobclarke92@gmail.com>
2016-09-07 04:35:50 +1000

    Mostly just made notes

--- src/timbre/constants/hookTheory.js
+++ src/timbre/constants/hookTheory.js
@@ -1,4 +1,8 @@
-// Reference: http://forum.hooktheory.com/t/trends-api-chord-input/272
+/**
+ * References:
+ * http://forum.hooktheory.com/t/trends-api-chord-input/272
+ * http://forum.hooktheory.com/t/vizualitation-of-all-chord-progressions-kinda/164/4
+ */
 
 export const modePrefixes = {
 	major: '',
@@ -7,25 +11,27 @@ export const modePrefixes = {
 	phrygian: 'Y',
 	lydian: 'L',
 	mixolydia: 'M',
+	aeolian: 'b',
 	locrian: 'C',
 }
 
-export const appliedPrefixes = {
-	'5/': 'over 5',
-	'4/': 'over 4',
-	'7/': 'over 7',
+export const appliedSuffixes = {
+	'/3': 'over 3',
+	'/4': 'over 4',
+	'/5': 'over 5',
+	'/7': 'over 7',
 }
 
 export const inversions = {
-	'6': '6',
-	'64': '64',
-	'65': '65',
-	'43': '43',
-	'42': '42',
+	'6': '6',    // <sup>6</sup>			 -- major 6
+	'64': '64',	 // <sup>6</sup><sub>4</sub> -- minor 6 ??
+	'65': '65',  // augmented ? 
+	'42': '42',	 // <sup>4</sup><sub>2</sub>
+	'43': '43',  // <sup>4</sup><sub>3</sub>
 }
 
 export const suffixes = {
-	'7' : '7',
+	'7' : '7', 
 }
 
 /**
@@ -35,4 +41,13 @@ export const suffixes = {
  * b5
  * b7    -- apparently is the same as 4/4 (4 over 4)
  * b67
+ *
+ * 242 	= ii<sup>4</sup><sub>2</sub>
+ * C164 = i°<sup>6</sup><sub>4</sub>
+ * Y6 	= ♭VI
+ * 7/5 	= vii°/V
+ * 77/5 = vii<sup>ø</sup><sup>7</sup> / V    -- ø = half diminished 7th chord
+ * M17	= Ib7
+ * D57 	= v<sup>7</sup>
+ * D47	= IVb7
  */
--- src/timbre/utils/hookTheoryUtils.js
+++ src/timbre/utils/hookTheoryUtils.js
@@ -1,16 +1,26 @@
-import { modePrefixes, appliedPrefixes, inversions } from '../constants/hookTheory'
+import { modePrefixes, appliedSuffixes, inversions, suffixes } from '../constants/hookTheory'
 
 const minorNumerals = [2,3,6];
+const modePrefixKeys = Object.keys(modePrefixes);
 
 export function chordStringToObject(str, scale) {
 	if(typeof str != 'string') str = str.toString();
 
 	const chord = {};
+	if(modePrefixKeys.indexOf(str.charAt(0)) >= 0) {
+		chord.mode = modePrefixes[str.charAt(0)];
+		str = str.substring(1);
+	}
 	if(str.length == 1) {
-		chord.numeral = parseInt(str);
-		chord.minor = minorNumerals.indexOf(parseInt(str)) >= 0;
+		const int = parseInt(str);
+		chord.numeral = int;
+		chord.type = (minorNumerals.indexOf(int) >= 0) ? 'minor' : 'major';
 	}
 	//etc etc....
 
 	return chord;
+}
+
+export function getNotesFromChord(chord, key = 0) {
+
 }
