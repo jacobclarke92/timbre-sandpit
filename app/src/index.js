8c403c5cf3598199fef8b975b38e0715b74bdfc6
Jacob Clarke <jacob@bigfish.tv>
2016-09-09 11:31:25 +1000

    Added midi dep

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,20 @@
 import electron, { app, BrowserWindow } from 'electron'
+import midi from 'midi'
 
 let mainWindow;
+let midiInput = new midi.input();
+let midiOutput = new midi.output();
 
 function createWindow() {
-	mainWindow = new BrowserWindow({width: 1280, height: 720});
+	
+	for(let i = 0; i < midiInput.getPortCount(); i ++) {
+		console.log('MIDI INPUT '+i+': '+midiInput.getPortName(i));
+	}
+	for(let i = 0; i < midiOutput.getPortCount(); i ++) {
+		console.log('MIDI OUTPUT '+i+': '+midiOutput.getPortName(i));
+	}
+
+	mainWindow = new BrowserWindow({width: 1920, height: 1080});
 	mainWindow.loadURL(`file://${__dirname}/../index.html`);
 	mainWindow.webContents.openDevTools();
 	mainWindow.on('closed', () => {
